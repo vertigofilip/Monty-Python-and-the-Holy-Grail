7f1044fa4610a8f1accb160931356083f7d9f5e4
vertigo.filip <vertigo.filip@gmail.com>
2023-03-29 12:48:31 +0200

    fixes for voice asistant

--- chatgpt_voice_asistant.py
+++ chatgpt_voice_asistant.py
@@ -1,21 +1,22 @@
-import speech_recognition as sr
-import pyttsx3
 import openai
-import os
+import pyttsx3
+import speech_recognition as sr
 import time
 
+# Set your OpenAI API key
 openai.api_key = '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'
 
+# Initialize the text-to-speach engine
 engine = pyttsx3.init()
 
 def trancribe_audio_to_text(filname):
-    recogniser=sr.Recognizer()
+    recognizer=sr.Recognizer()
     with sr.AudioFile(filname) as source:
-        audio=recogniser.record(source)
-        try:
-            return recogniser.recognize_google(audio)
-        except:
-            print('Skipping unknown error')
+        audio=recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print('Skipping unknown error')
 
 def generate_response(prompt):
     responce=openai.Completion.create(
@@ -34,24 +35,34 @@ def speak_text(text):
 
 def main():
     while True:
+        # Wait for user to say "chat"
         print('say "chat" to start secording your question...')
         with sr.Microphone() as source:
-            recogniser=sr.Recognizer()
-            audio=recogniser.listen(source)
+            recognizer=sr.Recognizer()
+            audio=recognizer.listen(source)
             try:
-                transcription=recogniser.recognize_google()
+                transcription=recognizer.recognize_google(audio)
                 if transcription.lower() == 'chat':
+                    # Record audio
                     filename='input.wav'
-                    recogniser=sr.Recognizer()
-                    source.pause_threshold=1
-                    audio=recogniser.listen(source, phrase_time_limit=None, timeout=None)
-                    with open(filename, 'wb') as f:
-                        f.write(audio.get_wav_data())
+                    print('Ask a question...')
+                    with sr.Microphone() as source:
+                        recognizer=sr.Recognizer()
+                        source.pause_threshold=1
+                        audio=recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, 'wb') as f:
+                            f.write(audio.get_wav_data())
+                
+                    # Transcribe audio to text
                     text=trancribe_audio_to_text(filename)
                     if text:
                         print(f'you said: {text}')
+
+                        # Generate response using GPT-3
                         response = generate_response(text)
                         print(f'GPT-3 says: {response}')
+
+                        # Readresponse using text-to-speach
                         speak_text(response)
             except Exception as e:
                 print('an error ocured: {}'.format(e))
